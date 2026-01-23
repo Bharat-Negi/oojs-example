9774cf956d7536819c22deb82d056f28299a2d35
Aman <rawat.aman1@gmail.com>
2026-01-23 17:58:47 +0530

    file update

--- Free-Test/script.js
+++ Free-Test/script.js
@@ -41,3 +41,59 @@ let course = {
 Object.entries(course).forEach(function (val) {
 	console.log(val[0] + ": " + val[1]);
 });
+
+// counter
+let count = 10;
+let timeHai = setInterval(function () {
+	if (count >= 1) {
+		console.log(count);
+		count--;
+	} else {
+		clearInterval(timeHai);
+		console.log(count);
+	}
+}, 1000);
+
+// this key word as per location
+console.log(this);
+function Hello() {
+	console.log(this);
+}
+Hello();
+
+let obj4 = {
+	name: "bharat",
+	age: 26,
+	mathArea: function () {
+		console.log(this);
+	},
+};
+obj4.mathArea();
+
+// fat arrow function this key word become window scope
+let obj5 = {
+	name: "bharat",
+	age: 26,
+	mathArea: () => {
+		console.log(this);
+	},
+};
+obj5.mathArea();
+
+// inside the function use only fat arrow function to remain this key word value
+let obj6 = {
+	name: "bharat",
+	age: 26,
+	mathArea: function () {
+		let negi = () => {
+			console.log(this);
+		};
+		negi();
+	},
+};
+obj6.mathArea();
+
+// event handler for this key word
+document.querySelector("h2").addEventListener("click", function () {
+	console.log(this);
+});
